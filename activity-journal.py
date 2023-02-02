1c88b594fc2ccce1cd69c509ae9fa11d7a2898fe
yokuha <124187637+yokuha@users.noreply.github.com>
2023-02-02 22:38:05 +0100

    Improve generated markdown

--- activity-journal.py
+++ activity-journal.py
@@ -22,11 +22,23 @@ with open(info_file, 'r', encoding='utf-8') as db:
 # create mardown text of all info in the current athlestes files
 mdFile = MdUtils(file_name=f'{athlete_id}.md', title=f'Activity journal of athlete {athlete_id} ({athlete["username"]})')
 mdFile.new_header(level=1, title='Athlete info')
-mdFile.new_header(level=1, title='Activity notes')
+mdFile.new_header(level=2, title='Activities')
 for id in info.keys():
     data = info[id]
-    print(data['name'])
-    mdFile.new_header(level=2, title=data['name'])
+    mdFile.new_paragraph()
+    mdFile.new_header(level=3, title=data['name'])
+    anynote = False
     if 'note' in data and None != data['note'] and len(data['note']) > 0:
-        mdFile.new_paragraph(data['note'])
+        mdFile.new_paragraph(data['note'].replace('\r', '   '))
+        mdFile.new_paragraph('---')
+        anynote = True
+    if 'private_note' in data and None != data['private_note'] and len(data['private_note']) > 0:
+        mdFile.new_paragraph()
+        mdFile.new_header(level=4, title='Private notes')
+        mdFile.new_paragraph(data['private_note'].replace('\r', '   '))
+        mdFile.new_paragraph('---')
+        anynote = True
+    if not anynote:
+        mdFile.new_paragraph('\ ')
+        mdFile.new_paragraph('---')
 mdFile.create_md_file()
